aadaa0d1a4a58b3d15d2c728b1e5d754f0ad1866
Talha <64659821+talhaozbek61@users.noreply.github.com>
2025-02-01 14:08:26 +0300

    chore: added react router v7 example files

---
+++ app/routes/edit-contact.tsx
@@ -0,0 +1,76 @@
+import { Form, redirect, useNavigate } from "react-router";
+import type { Route } from "./+types/edit-contact";
+
+import { getContact, updateContact } from "../data";
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return { contact };
+}
+
+export async function action({ request, params }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+}
+
+export default function EditContact({ loaderData }: Route.ComponentProps) {
+  const { contact } = loaderData;
+  const navigation = useNavigate();
+
+  return (
+    <Form key={contact.id} id="contact-form" method="post">
+      <p>
+        <span>Name</span>
+        <input
+          aria-label="First name"
+          defaultValue={contact.first}
+          name="first"
+          placeholder="First"
+          type="text"
+        />
+        <input
+          aria-label="Last name"
+          defaultValue={contact.last}
+          name="last"
+          placeholder="Last"
+          type="text"
+        />
+      </p>
+      <label>
+        <span>Github</span>
+        <input
+          defaultValue={contact.github}
+          name="github"
+          placeholder="@jack"
+          type="text"
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          aria-label="Avatar URL"
+          defaultValue={contact.avatar}
+          name="avatar"
+          placeholder="https://example.com/avatar.jpg"
+          type="text"
+        />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea defaultValue={contact.notes} name="notes" rows={6} />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button type="button" onClick={() => navigation(-1)}>
+          Cancel
+        </button>
+      </p>
+    </Form>
+  );
+}
